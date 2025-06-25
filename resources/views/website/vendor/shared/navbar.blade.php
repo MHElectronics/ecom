54750ea809e2859b46bdc15bc3a3fabcc11f1f8f
mahadishanto082 <mahadishanto26@gmail.com>
2025-06-25 15:24:45 +0600

    all set for nav bar

--- resources/views/website/vendor/shared/navbar.blade.php
+++ resources/views/website/vendor/shared/navbar.blade.php
@@ -1,48 +1,50 @@
-<div class="br-logo">
-    <a href="{{ route('vendor.dashboard') }}">
-        <span>[</span>Vendor<i>Panel</i><span>]</span>
-    </a>
-</div>
-
-<div class="br-sideleft sideleft-scrollbar" style="width: 180px; height:min-content"> {{-- Reduce width --}}
-    <label class="sidebar-label pd-x-10 mg-t-15 op-6 text-uppercase small">Menu</label>
-    <ul class="br-sideleft-menu">
-
-        {{-- Dashboard --}}
+<div class="br-logo"><a href="{{ route('vendor.dashboard') }}"><span>[</span>Order <i>Panel</i><span>]</span></a></div>
+<div class="br-sideleft sideleft-scrollbar" style="height: min-content;">
+    <label class="sidebar-label pd-x-10 mg-t-20 op-3">Navigation</label>
+    <ul class="br-sideleft-menu " >
         <li class="br-menu-item">
             <a href="{{ route('vendor.dashboard') }}"
-               class="br-menu-link {{ Request::routeIs('vendor.dashboard') ? 'active' : '' }}">
-                <i class="menu-item-icon icon ion-ios-home-outline tx-18"></i>
+               class="br-menu-link {{ Request::routeIs('vendor.dashboard*') ? 'active' : '' }}">
+                <i class="menu-item-icon icon ion-ios-home-outline tx-24"></i>
                 <span class="menu-item-label">Dashboard</span>
             </a>
         </li>
 
-        {{-- Products --}}
         <li class="br-menu-item">
             <a href="#"
-               class="br-menu-link {{ Request::routeIs('vendor.products') ? 'active' : '' }}">
-                <i class="menu-item-icon icon ion-pricetag tx-18"></i>
+               class="br-menu-link with-sub {{ Request::routeIs('vendor.categories*') ? 'active show-sub' : ''  }}">
+                <i class="menu-item-icon icon ion-cube tx-20"></i>
                 <span class="menu-item-label">Products</span>
             </a>
+           
         </li>
 
-        {{-- Orders --}}
         <li class="br-menu-item">
-            <a href="#"
-               class="br-menu-link {{ Request::routeIs('#') ? 'active' : '' }}">
-                <i class="menu-item-icon icon ion-ios-cart-outline tx-18"></i>
+            <a href="#" class="br-menu-link with-sub {{ Request::routeIs('#') ? 'active show-sub' : ''  }}">
+                <i class="menu-item-icon icon ion-laptop tx-20"></i>
                 <span class="menu-item-label">Orders</span>
             </a>
+            
         </li>
 
-        {{-- Create Product --}}
         <li class="br-menu-item">
             <a href="#"
-               class="br-menu-link {{ Request::routeIs('#') ? 'active' : '' }}">
-                <i class="menu-item-icon icon ion-plus-round tx-18"></i>
-                <span class="menu-item-label">Create</span>
+               class="br-menu-link with-sub {{ Request::routeIs('#') ? 'active show-sub' : ''  }}">
+                <i class="menu-item-icon icon ion-report tx-20"></i>
+                <span class="menu-item-label">Sells report</span>
             </a>
+           
         </li>
+        <li class="br-menu-item">
+            <a href="#"
+               class="br-menu-link with-sub {{ Request::routeIs('#') ? 'active show-sub' : ''  }}">
+                <i class="menu-item-icon icon ion-edit tx-20"></i>
+                <span class="menu-item-label">Create Product</span>
+            </a>
+           
+        </li>
+
+       
     </ul>
     <br>
 </div>
